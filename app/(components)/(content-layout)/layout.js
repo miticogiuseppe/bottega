3d94a3f036143c7cdadd3a323eda1a09e4bfb296
miticogiuseppe <miticogiuseppe@gmail.com>
2025-11-04 16:57:47 +0100

    Headers sistemati

--- app/(components)/(content-layout)/layout.js
+++ app/(components)/(content-layout)/layout.js
@@ -1,38 +1,40 @@
-"use client"
-import Footer from '../../../shared/layouts-components/footer/footer';
-import Header from '../../../shared/layouts-components/header/header';
-import Sidebar from '../../../shared/layouts-components/sidebar/sidebar';
-import Switcher from '../../../shared/layouts-components/switcher/switcher';
-import React, { Fragment } from 'react'
-import { connect } from "react-redux"
-import { ThemeChanger } from "../../../shared/redux/action"
-import Backtotop from '../../../shared/layouts-components/backtotop/backtotop';
-import Loader from '../../../shared/layouts-components/loader/loader';
+"use client";
+import Footer from "../../../shared/layouts-components/footer/footer";
+import Header from "../../../shared/layouts-components/header/header";
+import Sidebar from "../../../shared/layouts-components/sidebar/sidebar";
+import Switcher from "../../../shared/layouts-components/switcher/switcher";
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
+import { ThemeChanger } from "../../../shared/redux/action";
+import Backtotop from "../../../shared/layouts-components/backtotop/backtotop";
+import Loader from "../../../shared/layouts-components/loader/loader";
 
-const Layout = ({ children, }) => {
+const Layout = ({ children }) => {
+  return (
+    <Fragment>
+      <Switcher />
+      <Loader />
+      <div className="page">
+        <Header
+          showProfile={false}
+          showLanguages={false}
+          showCart={false}
+          showNotifications={false}
+        />
 
-    return (
-        <Fragment>
-            <Switcher />
-            <Loader/>
-            <div className='page'>
-                <Header />
-                <Sidebar />
-                <div className='main-content app-content'>
-                    <div className='container-fluid'>
-                        {children}
-                    </div>
-                </div>
-                <Footer />
-            </div>
-            <Backtotop />
-        </Fragment>
-    )
-
-}
+        <Sidebar />
+        <div className="main-content app-content">
+          <div className="container-fluid">{children}</div>
+        </div>
+        <Footer />
+      </div>
+      <Backtotop />
+    </Fragment>
+  );
+};
 
 const mapStateToProps = (state) => ({
-    local_varaiable: state
+  local_varaiable: state,
 });
 
 export default connect(mapStateToProps, { ThemeChanger })(Layout);
